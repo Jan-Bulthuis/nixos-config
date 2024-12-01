34bba528b3291ab04654348d424fbd7309a6b62d
Jan-Bulthuis <git@bulthuis.dev>
2024-12-02 00:52:41 +0100

    Added JS language support

---
+++ modules/users/modules/language/js.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.modules.js;
+in
+{
+  options.modules.js = {
+    enable = mkEnableOption "js";
+  };
+
+  config = mkIf cfg.enable {
+    # Development packages
+    home.packages = with pkgs; [
+      nodejs
+      tailwindcss
+    ];
+
+    # VSCode configuration
+    programs.vscode = {
+      extensions = with pkgs.vscode-extensions; [
+        bradlc.vscode-tailwindcss
+      ];
+
+      userSettings = { };
+    };
+
+    # Neovim configuration
+    programs.nixvim = { };
+  };
+}
