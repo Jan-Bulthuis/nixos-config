ac628036a94e40d6db577f0c0f64479ee3008a50
Jan-Bulthuis <git@bulthuis.dev>
2024-12-02 00:54:51 +0100

    Added steam

--- modules/users/modules/systemwide/steam.nix
+++ modules/users/modules/systemwide/steam.nix
@@ -10,5 +10,13 @@ let
   enabled = any (user: user.modules.steam.enable) (attrValues config.home-manager.users);
 in
 {
-  config = mkIf enabled { programs.steam.enable = true; };
+  config = mkIf enabled {
+    modules.unfree.allowedPackages = [
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+    ];
+
+    programs.steam.enable = true;
+  };
 }
