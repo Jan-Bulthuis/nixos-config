727c0802a7b4ea7a141cfdb8a7a4fcb5739e7e26
Jan-Bulthuis <git@bulthuis.dev>
2024-12-03 13:46:43 +0100

    Fixed nerd font import

--- modules/users/modules/theming/fonts/nerd-fonts-symbols.nix
+++ modules/users/modules/theming/fonts/nerd-fonts-symbols.nix
@@ -2,7 +2,7 @@
 
 {
   name = "Symbols Nerd Font Mono";
-  package = pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; };
+  package = pkgs.nerd-fonts.symbols-only;
   recommendedSize = 12;
   fallbackFonts = [ ];
 }
