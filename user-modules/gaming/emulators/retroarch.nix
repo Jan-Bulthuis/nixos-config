dec735c9d65ac597e59cc9093d624ecf3b2bc255
Jan-Bulthuis <git@bulthuis.dev>
2025-02-08 21:51:46 +0100

    Added unfree retroarch emulators

--- user-modules/gaming/emulators/retroarch.nix
+++ user-modules/gaming/emulators/retroarch.nix
@@ -15,8 +15,27 @@ in
   };
 
   config = mkIf cfg.enable {
+    modules.unfree.allowedPackages = [
+      "libretro-fbalpha2012"
+      "libretro-fbneo"
+      "libretro-fmsx"
+      "libretro-genesis-plus-gx"
+      "libretro-mame2000"
+      "libretro-mame2003"
+      "libretro-mame2003-plus"
+      "libretro-mame2010"
+      "libretro-mame2015"
+      "libretro-opera"
+      "libretro-picodrive"
+      "libretro-snes9x"
+      "libretro-snes9x2002"
+      "libretro-snes9x2005"
+      "libretro-snes9x2005-plus"
+      "libretro-snes9x2010"
+    ];
+
     home.packages = with pkgs; [
-      retroarch-free
+      retroarch-full
     ];
   };
 }
