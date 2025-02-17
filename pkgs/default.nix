07c056b0cd3e61be6465173e6c0669da0e77ed07
Jan-Bulthuis <git@bulthuis.dev>
2025-02-17 12:51:56 +0100

    Fixed package import name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@
 
 let
   overlay = final: prev: {
-    dina-psf = prev.callPackage ./fonts/dina-psf.nix { };
+    dina-psfu = prev.callPackage ./fonts/dina-psfu.nix { };
     ttf2psf = prev.callPackage ./fonts/ttf2psf.nix { };
     wqy-zenhei = prev.callPackage ./fonts/wqy-zenhei.nix { };
     wqy-microhei = prev.callPackage ./fonts/wqy-microhei.nix { };
