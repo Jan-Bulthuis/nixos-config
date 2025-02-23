f224a5efa73b8ae04c4efcc602bb6fb093b8e2b0
Jan-Bulthuis <git@bulthuis.dev>
2025-02-23 14:52:07 +0100

    Set default terminal

--- user-modules/terminal/default.nix
+++ user-modules/terminal/default.nix
@@ -9,6 +9,9 @@ with lib;
 {
   options.default.terminal = mkOption {
     type = types.str;
+    # TODO: Make sure everything works even without a default value here
+    # Maybe make sure most gui applications do not exist in the default specialisation
+    default = "foot";
     description = "Default terminal application";
   };
 }
