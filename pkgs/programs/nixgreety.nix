3619713bf295929b99860d2c48f65ada58040d95
Jan-Bulthuis <git@bulthuis.dev>
2025-02-27 11:12:01 +0100

    Updated nixgreety

--- pkgs/programs/nixgreety.nix
+++ pkgs/programs/nixgreety.nix
@@ -9,8 +9,8 @@ rustPlatform.buildRustPackage {
     domain = "git.bulthuis.dev";
     owner = "Jan";
     repo = "nixgreety";
-    rev = "4b5f812e95f6359ce61aef685005dc4013f0fb5f";
-    hash = "sha256-/eikqX/2byDi04v1XvsBVva1Vs7OGLl/cC/Vrq+QF9A=";
+    rev = "c7278a910a0238a53f23fe9a0ae881802a4bcb31";
+    hash = "sha256-kZB+iEFIDJ8pOJetu4Isu4oaktgIn14D4PcpDXLOXA8=";
   };
 
   cargoHash = "sha256-pklKVzYoChRqPZ/D3BsMGnaBFd615TKbvoAy7iU8UtA=";
