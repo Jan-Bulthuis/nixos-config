18db7c616d89b76b4b39b9f56a6c7c5687ec6b7a
Jan-Bulthuis <git@bulthuis.dev>
2025-04-16 20:01:02 +0200

    Added rtkit for pipewire

--- modules/sound/pipewire.nix
+++ modules/sound/pipewire.nix
@@ -15,9 +15,11 @@ in
   };
 
   config = mkIf cfg.enable {
+    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
     };
