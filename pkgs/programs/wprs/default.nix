26a4cba43cbd7e6f4c57c73ef738547a6e8f9847
Jan-Bulthuis <git@bulthuis.dev>
2025-04-18 10:22:52 +0200

    Moved wprs to a debug version

--- pkgs/programs/wprs/default.nix
+++ pkgs/programs/wprs/default.nix
@@ -11,6 +11,8 @@
 rustPlatform.buildRustPackage {
   pname = "wprs";
   version = "0-unstable-2025-04-16";
+  buildType = "debug";
+  dontStrip = true;
 
   src = fetchFromGitHub {
     owner = "wayland-transpositor";
