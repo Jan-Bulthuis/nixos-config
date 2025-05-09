2b0bdb0248c9dd77dd72c27414f25531327d4a70
Jan-Bulthuis <git@bulthuis.dev>
2025-05-09 15:01:07 +0200

    Enabled grdp correctly by adding GDM

--- modules/grdp/default.nix
+++ modules/grdp/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 {
@@ -8,11 +13,29 @@ with lib;
 
   config = mkIf config.modules.grdp.enable {
     services.gnome.gnome-remote-desktop.enable = true;
+    environment.systemPackages = with pkgs; [
+      gnome-remote-desktop
+      gnome-control-center
+      gnome-session
+      gnome-shell
+      gnome-settings-daemon
+      gdm
+    ];
     systemd.services."gnome-remote-desktop".wantedBy = [ "graphical.target" ];
     networking.firewall = {
       allowedTCPPorts = [ 3389 ];
       allowedUDPPorts = [ 3389 ];
     };
+    services.xserver.displayManager.gdm.enable = true;
+    modules.greetd.enable = mkForce false;
+
+    # security.polkit.extraConfig = ''
+    #   polkit.addRule(function(action, subject) {
+    #     if (action.id == "org.gnome.controlcenter.remote-session-helper" &&
+    #         subject.isInGroup("wheel")) {
+    #       return polkit.Result.YES;
+    #     }
+    #   });'';
     # programs.dconf.profiles.user.databases = [
     #   {
     #     settings = with lib.gvariant; {
