76e609372f7a1f0fa3a43ecb8c39bc39620120ce
Jan-Bulthuis <git@bulthuis.dev>
2025-05-29 20:28:07 +0200

    Added persistence to ssh host keys

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -11,5 +11,17 @@ in
   config = mkIf cfg.enable {
     services.openssh.enable = true;
     # TODO: Is this default configuration secure?
+
+    services.openssh.hostKeys = mkIf (config.modules.impermanence.enable) [
+      {
+        type = "ed25519";
+        path = "/persist/system/etc/ssh/ssh_host_ed25519_key";
+      }
+      {
+        type = "rsa";
+        bits = 4096;
+        path = "/persist/system/etc/ssh/ssh_host_rsa_key";
+      }
+    ];
   };
 }
