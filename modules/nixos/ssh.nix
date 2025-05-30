0cf53a97cf67da1bcdba10119cd6999dff36f554
Jan-Bulthuis <git@bulthuis.dev>
2025-05-30 16:15:52 +0200

    Restricted SSH access

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -9,19 +9,24 @@ in
     enable = mkEnableOption "ssh";
   };
   config = mkIf cfg.enable {
-    services.openssh.enable = true;
-    # TODO: Is this default configuration secure?
-
-    services.openssh.hostKeys = mkIf (config.modules.impermanence.enable) [
-      {
-        type = "ed25519";
-        path = "/persist/system/etc/ssh/ssh_host_ed25519_key";
-      }
-      {
-        type = "rsa";
-        bits = 4096;
-        path = "/persist/system/etc/ssh/ssh_host_rsa_key";
-      }
-    ];
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+      hostKeys = mkIf (config.modules.impermanence.enable) [
+        {
+          type = "ed25519";
+          path = "/persist/system/etc/ssh/ssh_host_ed25519_key";
+        }
+        {
+          type = "rsa";
+          bits = 4096;
+          path = "/persist/system/etc/ssh/ssh_host_rsa_key";
+        }
+      ];
+    };
   };
 }
