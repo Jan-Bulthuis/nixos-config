32e7d99292d9a2f89c82c6a471591e3763601f42
Jan-Bulthuis <git@bulthuis.dev>
2025-06-09 13:54:31 +0200

    Set up request-key.conf

--- hosts/vm-oddjob/configuration.nix
+++ hosts/vm-oddjob/configuration.nix
@@ -24,6 +24,26 @@
     samba
     keyutils
   ];
+  environment.etc."request-key.conf".text =
+    let
+      upcall = "${pkgs.cifs-utils}/bin/cifs.upcall";
+      keyctl = "${pkgs.keyutils}/bin/keyctl";
+    in
+    ''
+      #OP    TYPE         DESCRIPTION  CALLOUT_INFO PROGRAM
+      # -t is required for DFS share servers...
+      create cifs.spnego  *            *            ${upcall} -t %k
+      create dns_resolver *            *            ${upcall} %k
+      # Everything below this is essentially the
+      # defualt configuration
+      create user         debug:*      negate       ${keyctl} negate %k 30 %S
+      create user         debug:*      rejected     ${keyctl} reject %k 30 %c %S
+      create user         debug:*      expired      ${keyctl} reject %k 30 %c %S
+      create user         debug:*      revoked      ${keyctl} reject %k 30 %c %S
+      create user         debug:loop:* *            |${pkgs.coreutils}/bin/cat
+      create user         debug:*      *            ${pkgs.keyutils}/share/keyutils/request-key-debug.sh %k %d %c %S
+      negate *            *            *            ${keyctl} negate %k 30 %S
+    '';
   sops.secrets."smb-credentials" = {
     sopsFile = "${inputs.secrets}/secrets/vm-oddjob.enc.yaml";
   };
