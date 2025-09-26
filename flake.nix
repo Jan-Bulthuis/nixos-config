d3b3e9be1cad875c7a0c707ad5d51075ed4ba8ef
Jan-Bulthuis <git@bulthuis.dev>
2025-09-26 08:28:14 +0200

    Updated flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # General inputs
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
