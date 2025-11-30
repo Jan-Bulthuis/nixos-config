c3014ec109c2f3deb6e7e39e23af1a63fc28f20d
Jan-Bulthuis <git@bulthuis.dev>
2025-11-30 12:27:42 +0100

    chore: Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # General inputs
-    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
